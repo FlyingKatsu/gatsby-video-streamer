b37e51d5ce5cb66fd979eeb5d788174792dbe89a
Flying Katsu <Warped2713@users.noreply.github.com>
2019-01-18 14:14:30 -0500

    barebones video page and index

---
+++ src/pages/index.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+
+import Bio from '../component/Bio'
+import Layout from '../component/Layout'
+import SEO from '../component/SEO'
+
+class VideoIndex extends React.Component {
+    render() {
+        const { data } = this.props
+        const siteTitle = data.site.siteMetadata.title
+        const videos = data.videos.edges
+        const thumbs = data.thumbs ? data.thumbs.edges : null // TODO: make component for rendering thumbnails
+
+        return (
+            <Layout location={this.props.location} title={siteTitle}>
+                <SEO
+                    title="All videos"
+                    keywords={[`video`, `gatsby`, `javascript`, `react`]}
+                />
+                <Bio avatar={data.avatar} />
+                {videos.map(({ node }) => {
+                    const title = node.name || node.fields.slug
+                    return (
+                        <div key={node.fields.slug}>
+                            <h3>
+                                <Link to={node.fields.slug}>
+                                    {title}
+                                </Link>
+                                {' '}- <small><time dateTime={node.modifiedTime}>{node.modifiedTime}</time></small>
+                            </h3>
+                            {thumbs ? <img src={thumbs[0].publicURL} /> : ''}
+                        </div>
+                    )
+                })}
+            </Layout>
+        )
+    }
+}
+
+export default VideoIndex
+
+export const pageQuery = graphql`
+  query getVideoIndex($avatar: String!) {
+    site: site {
+      siteMetadata {
+        title
+      }
+    }
+    avatar: file(relativePath: { eq: $avatar }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    videos: allFile(
+        filter: { sourceInstanceName: { eq: "video" } }
+        sort: { fields: modifiedTime, order: DESC }
+        limit: 20
+    ) {
+      edges {
+        node {
+          extension
+          publicURL
+          modifiedTime(formatString: "MMMM DD, YYYY")
+          name
+          fields {
+            slug
+          }
+        }
+      }
+    }
+    thumbs: allFile(
+        filter: { sourceInstanceName: { eq: "thumb" } }
+        limit: 4
+    ) {
+      edges {
+        node {
+          extension
+          publicURL
+          name
+          relativeDirectory
+        }
+      }
+    }
+  }
+`
---
+++ src/template/VideoPage.js
@@ -0,0 +1,167 @@
+import React from 'react'
+import { Link, graphql, withPrefix } from 'gatsby'
+
+import Bio from '../component/Bio'
+import Layout from '../component/Layout'
+import SEO from '../component/SEO'
+
+class VideoPageTemplate extends React.Component {
+
+    constructor(props) {
+        super(props)
+        console.log(props)
+    }
+
+    getVideoPlayer(video,title,thumb) {
+        if (['video/mp4','video/ogg','video/webm'].indexOf(video.internal.mediaType) < 0) {
+            return (
+                <div>
+                    {(thumb) ? (<img src={withPrefix(thumb.publicURL)} width={640} height={360} />) : ''}
+                    <p>HTML5 Video player does not support the <b>.{video.extension}</b> file type, 
+                        but you can still download the video to watch on your own video player:{' '}
+                        <a href={withPrefix(video.publicURL)} title={`Download ${title} video`}>
+                            {`${video.name}.${video.extension}`}</a> ({video.prettySize})</p>
+                </div>
+            )
+        }
+        return (
+            <video width={640} height={360} controls autoPlay playsinline>
+                <source src={withPrefix(video.publicURL)} type={video.internal.mediaType} />
+                {`Your browser does not currently support the HTML5 <video> tag`}
+            </video>
+        )
+    }
+
+  render () {
+    const site = this.props.data.site;
+    const video = this.props.data.video;
+    const dash = this.props.data.dash;
+    const hls = this.props.data.hls;
+    const thumb = this.props.data.thumb; // TODO: create a component for rendering one thumb at a time
+    const post = this.props.data.post;
+
+    const videoDetail = {
+        title: 'Untitled',
+        desc: `video shared by ${site.siteMetadata.author}`,
+        content: {__html: '<p>No description</p>'},
+    }
+
+    if (post) {
+        if (post.frontmatter) {
+            videoDetail.title = post.frontmatter.title || videoDetail.title
+            videoDetail.desc = post.frontmatter.decription || `${videoDetail.title} ${videoDetail.desc}`
+        }
+        videoDetail.content = {__html: post.html} || videoDetail.content
+    }
+
+    const siteTitle = site.siteMetadata.title
+    const { slug, name, previous, next } = this.props.pageContext
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title={videoDetail.title} description={videoDetail.desc} />
+        <h1>{videoDetail.title}</h1>
+        <div>
+            {this.getVideoPlayer(video,videoDetail.title,thumb)}
+        </div>
+        <div dangerouslySetInnerHTML={videoDetail.content}></div>
+        <hr/>
+        <Bio avatar={this.props.data.avatar}/>
+
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.name}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.name} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </Layout>
+    )
+  }
+}
+
+export default VideoPageTemplate
+
+export const pageQuery = graphql`
+query VideoBySlug($slug: String!, $name: String!, $avatar: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+        description
+      }
+    }
+    avatar: file(relativePath: { eq: $avatar }) {
+        childImageSharp {
+          fixed(width: 50, height: 50) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    video: file(fields: {slug: {eq: $slug}}) {
+      extension
+      modifiedTime(formatString: "MMMM DD, YYYY")
+      name
+      prettySize
+      publicURL
+      internal {
+          mediaType
+      }
+    }
+    dash: file(
+      sourceInstanceName: {eq: "vid-dash"}
+      relativeDirectory: {eq: $name}
+    ) {
+      base
+      relativeDirectory
+      relativePath
+    }
+    hls: file(
+      sourceInstanceName: {eq: "vid-hls"}
+      relativeDirectory: {eq: $name}
+    ) {
+      base
+      relativeDirectory
+      relativePath
+    }
+    thumb: file(
+      sourceInstanceName: {eq: "thumb"}
+      relativeDirectory: {eq: $name}
+      ) {
+      publicURL
+    }
+    post: markdownRemark(
+        fields: { collection: {eq: "md-video-detail"} }
+        frontmatter: { name: {eq: $name} }
+      ) {
+        id
+        excerpt(pruneLength: 160)
+        html
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+          publish
+          patreontier
+          category
+          tagged
+        }
+    }
+  }
+`
